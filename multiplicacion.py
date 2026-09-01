9a4be2eaeeb2e6962c129b1fe57fab7dcbb06d99
Alexander Tzoc <jealtzo26@gmail.com>
2026-08-31 20:11:45 -0600

    feat: Add multiplicacion

---
+++ multiplicacion.py
@@ -0,0 +1,5 @@
+def mostrar_menu_multiplicacion():
+    print("\n--- Módulo de Multiplicación ---")
+    print("Esta es una funcionalidad descriptiva:")
+    print("Aquí se implementarán las multiplicaciones")
+    print("---------------------------------------\n")
