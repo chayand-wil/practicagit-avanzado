7d82322031891e9c1d8677d58ae30c4d672e05f3
Byron Torres <ajxupbyron@gmail.com>
2026-08-31 21:54:51 -0600

    refactor: usar variable total

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
 def calcular_total(precio, cantidad):
     """Calcula el total de una compra."""
-    return precio * cantidad
+    total = precio * cantidad
+    return total
 
 print(calcular_total(5, 2))
